76f7ef8aa3dd8e67352c94b6c26d66f2b79178c0
verysuper <verysuper@live.com>
2018-06-04 21:13:04 +0800

    0601c

---
+++ php_student/insert.php
@@ -0,0 +1,94 @@
+<?php require_once('Connections/studentdb.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1") && ($_POST["studno"] != "")) {
+  $insertSQL = sprintf("INSERT INTO student (studno, name, phone, address) VALUES (%s, %s, %s, %s)",
+                       GetSQLValueString($_POST['studno'], "text"),
+                       GetSQLValueString($_POST['name'], "text"),
+                       GetSQLValueString($_POST['phone'], "text"),
+                       GetSQLValueString($_POST['address'], "text"));
+
+  mysql_select_db($database_studentdb, $studentdb);
+  $Result1 = mysql_query($insertSQL, $studentdb) or die(mysql_error());
+
+  $insertGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無標題文件</title>
+</head>
+
+<body>
+<form action="<?php echo $editFormAction; ?>" id="form1" name="form1" method="POST">
+        <p>
+        <label for="studno">學號:</label>
+        <input type="text" name="studno" id="studno">
+        </p>
+        <p>
+        <label for="name">姓名:</label>
+        <input type="text" name="name" id="name">
+        </p>
+        <p>
+        <label for="phone">電話:</label>
+        <input type="text" name="phone" id="phone">
+        </p>
+        <p>
+        <label for="address">地址:</label>
+        <input type="text" name="address" id="address">
+        </p>
+        <input type="submit" value="新增學生資料" onClick="checkvalue()">
+        <input type="hidden" name="MM_insert" value="form1">
+    </form>
+    <script>
+    	function checkvalue(){
+				if(form1.studno.value == ''){
+					alert('請輸入學號');
+					return false;
+				}
+			}
+    </script>
+</body>
+</html>
--- php_student/score_read.php
+++ php_student/score_read.php
@@ -31,11 +31,44 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
+$currentPage = $_SERVER["PHP_SELF"];
+
+$maxRows_Recordset1 = 10;
+$pageNum_Recordset1 = 0;
+if (isset($_GET['pageNum_Recordset1'])) {
+  $pageNum_Recordset1 = $_GET['pageNum_Recordset1'];
+}
+$startRow_Recordset1 = $pageNum_Recordset1 * $maxRows_Recordset1;
+
 mysql_select_db($database_studentdb, $studentdb);
 $query_Recordset1 = "SELECT * FROM score";
-$Recordset1 = mysql_query($query_Recordset1, $studentdb) or die(mysql_error());
+$query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
+$Recordset1 = mysql_query($query_limit_Recordset1, $studentdb) or die(mysql_error());
 $row_Recordset1 = mysql_fetch_assoc($Recordset1);
-$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+if (isset($_GET['totalRows_Recordset1'])) {
+  $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
+} else {
+  $all_Recordset1 = mysql_query($query_Recordset1);
+  $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
+}
+$totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
+
+$queryString_Recordset1 = "";
+if (!empty($_SERVER['QUERY_STRING'])) {
+  $params = explode("&", $_SERVER['QUERY_STRING']);
+  $newParams = array();
+  foreach ($params as $param) {
+    if (stristr($param, "pageNum_Recordset1") == false && 
+        stristr($param, "totalRows_Recordset1") == false) {
+      array_push($newParams, $param);
+    }
+  }
+  if (count($newParams) != 0) {
+    $queryString_Recordset1 = "&" . htmlentities(implode("&", $newParams));
+  }
+}
+$queryString_Recordset1 = sprintf("&totalRows_Recordset1=%d%s", $totalRows_Recordset1, $queryString_Recordset1);
 ?>
 <!doctype html>
 <html>
@@ -62,6 +95,15 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
     <td><?php echo $row_Recordset1['math']; ?></td>
   </tr>
   <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
+  <tr>
+  	<td colspan="2">
+    <a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, 0, $queryString_Recordset1); ?>">[第一頁]</a> 
+    <a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, max(0, $pageNum_Recordset1 - 1), $queryString_Recordset1); ?>">[上一頁]</a> 
+    <a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, min($totalPages_Recordset1, $pageNum_Recordset1 + 1), $queryString_Recordset1); ?>">[下一頁]</a> 
+    <a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, $totalPages_Recordset1, $queryString_Recordset1); ?>">[最後一頁]</a>
+    </td>
+  	<td colspan="3">第 <?php echo ($startRow_Recordset1 + 1) ?> 筆至第 <?php echo min($startRow_Recordset1 + $maxRows_Recordset1, $totalRows_Recordset1) ?> 筆/共 <?php echo $totalRows_Recordset1 ?>  筆</td>
+  </tr>
 </table>
 </body>
 </html>
