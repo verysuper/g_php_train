d0a898acae5c9742e5137257be0bf767e95a6f03
verysuper <verysuper@live.com>
2018-06-08 21:06:35 +0800

    0608_2

---
+++ php_incomplete/Shop/createTitle.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/Shop.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<script language="javascript" src="../Scripts/menu.js"/></script>
+<head>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>伴手網</title>
+<!-- InstanceEndEditable -->
+
+<!-- Meta Tags -->
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+<meta name="robots"      content="index, follow" />
+<meta name="description" content="" />
+<meta name="keywords"    content="" />
+<meta name="author"      content="" />
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="" />
+
+<!-- CSS -->
+<link rel="stylesheet" href="../css/styles.css" media="all" type="text/css" />
+
+<!-- RSS -->
+<link rel="alternate" href="" title="RSS Feed" type="application/rss+xml" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+<body>
+  <div id="header">
+      <div class="subContainer">
+            <div id="logo">
+            <div id="box">H2H</div>
+            <p>咱的好伴手</p>
+            </div><!-- /logo -->
+    </div><!-- /subContainer -->
+</div><!-- header -->
+    
+<div id="navigation">   
+	<ul id="sddm">
+        <li><a href="index.php">首頁</a></li>
+        <li><a href="product.php">商品資料</a></li>
+        <li><a href="cart.php">購物車</a></li>
+        <li><a href="checkOut.php">結帳</a></li>     
+        <li><a href="guestBook.php">留言板</a></li>        
+        <li><a href="titleView.php">討論區</a></li>
+	    <li><a href="#" 
+        onmouseover="mopen('m1')" 
+        onmouseout="mclosetime()">會員中心</a>
+        <div id="m1" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="reconfirm.php">重發確認信</a>
+        <a href="modifydata.php">修改基本資料</a>
+        <a href="forgetpassword.php">忘記密碼</a>
+        </div>
+    	</li>
+	    <li><a href="#" 
+        onmouseover="mopen('m2')" 
+        onmouseout="mclosetime()">公佈欄</a>
+        <div id="m2" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="publishBrowser.php">瀏覽公佈欄</a>
+        </div>
+    	</li>
+		</ul> 
+<div style="clear:both"></div>    
+</div><!-- /navigation -->
+<object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="780" height="120">
+    <param name="movie" value="../Scripts/pic.swf" />
+    <param name="quality" value="high" />
+    <param name="wmode" value="opaque" />
+    <param name="swfversion" value="8.0.35.0" />
+
+    <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+    <!--[if !IE]>-->
+    <object type="application/x-shockwave-flash" data="../Scripts/pic.swf" width="780" height="120">
+      <!--<![endif]-->
+      <param name="quality" value="high" />
+      <param name="wmode" value="opaque" />
+      <param name="swfversion" value="8.0.35.0" />
+      <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+      <div>
+        <h4>這個頁面上的內容需要較新版本的 Adobe Flash Player。</h4>
+        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="取得 Adobe Flash Player" width="112" height="33" /></a></p>
+      </div>
+      <!--[if !IE]>-->
+    </object>
+    <!--<![endif]-->
+  </object>
+<div id="container"><!-- InstanceBeginEditable name="EditRegion1" -->
+  <div id="primaryContent">
+    <h2>討論區</h2>
+    <h3>歡迎您在此提出您的任何疑問，與大家一同討論！</h3>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <form action="" method="post" id="form1">
+    <table>
+    <tr valign="baseline">
+    <td align="right">姓名:</td>
+    <td colspan="2"><input type="text" name="name" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+    <td align="right">電子郵件:</td>
+    <td colspan="2"><input type="text" name="email" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+    <td align="right">主題:</td>
+    <td colspan="2"><input type="text" name="subject" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+    <td align="right" valign="top">內容:</td>
+    <td colspan="2"><textarea name="memo" cols="46" rows="5"></textarea></td>
+    </tr>
+    <tr valign="baseline">
+    <td align="right">&nbsp;</td>
+    <td colspan="2"><input type="submit" value="新增討論主題" />
+    </tr>
+    </table> 
+    <input type="hidden" name="createDate" value="<?php echo date("Y-m-d H:i:s"); ?>">
+    <input type="hidden" name="lastNewsDate" value="<?php echo date("Y-m-d H:i:s"); ?>">
+    <input type="hidden" name="count" value="0">
+    </form>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <!-- /comments -->
+  </div>
+<!-- InstanceEndEditable -->
+  <!-- /primaryContent -->
+  <div id="secondaryContent">
+    <p>&nbsp;</p>
+    <h3>最新商品</h3>
+    <h3>相關連結</h3>
+    <!-- 
+            This part was designed to handle images
+            -->
+    <ul class="links">
+      <li><a href="http://www.pcschool.com.tw/">巨匠電腦</a></li>
+      <li><a href="http://kid.pcschool.com.tw/">兒童電腦</a></li>      
+      <li><a href="http://www.soeasyedu.com.tw/">數位學習</a></li>
+      <li><a href="http://www.soeasyedu.com.tw/">巨匠美語</a></li>  
+      <li><a href="http://www.off.com.tw/weiblog">給我建議</a>
+    </ul>
+    <h3>登入</h3>
+    <form action="" method="post">
+      <p>
+        <input value="user" name="user" />
+      </p>
+      <p>
+        <input name="password" type="password" value="password" />
+      </p>
+        <button type="submit">送出</button><button>註冊會員</button>        
+    </form>    
+  </div>
+  <!-- /secondaryContent -->
+        <br class="clear" />    
+</div><!-- container -->
+    
+    <div id="footer">
+        <ul>
+            <li id="copyright">&copy; 2099 Free for Gjun student member</li>
+            <li id="links">
+                <ul>
+                    <li><a href="#">XHTML</a> |</li>
+                    <li><a href="#">CSS</a> |</li>
+                    <li><a href="http://gjun.tw">ALAN CHEN</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div><!-- /footer -->
+</body>
+<!-- InstanceEnd --></html>
---
+++ php_incomplete/Shop/titleView.php
@@ -0,0 +1,233 @@
+<?php require_once('../Connections/shop.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$maxRows_Recordset1 = 3;
+$pageNum_Recordset1 = 0;
+if (isset($_GET['pageNum_Recordset1'])) {
+  $pageNum_Recordset1 = $_GET['pageNum_Recordset1'];
+}
+$startRow_Recordset1 = $pageNum_Recordset1 * $maxRows_Recordset1;
+
+mysql_select_db($database_shop, $shop);
+$query_Recordset1 = "SELECT * FROM titles ORDER BY createDate DESC";
+$query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
+$Recordset1 = mysql_query($query_limit_Recordset1, $shop) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+
+if (isset($_GET['totalRows_Recordset1'])) {
+  $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
+} else {
+  $all_Recordset1 = mysql_query($query_Recordset1);
+  $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
+}
+$totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/Shop.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<script language="javascript" src="../Scripts/menu.js"/></script>
+<head>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>伴手網</title>
+<!-- InstanceEndEditable -->
+
+<!-- Meta Tags -->
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+<meta name="robots"      content="index, follow" />
+<meta name="description" content="" />
+<meta name="keywords"    content="" />
+<meta name="author"      content="" />
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="" />
+
+<!-- CSS -->
+<link rel="stylesheet" href="../css/styles.css" media="all" type="text/css" />
+
+<!-- RSS -->
+<link rel="alternate" href="" title="RSS Feed" type="application/rss+xml" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+<body>
+  <div id="header">
+      <div class="subContainer">
+            <div id="logo">
+            <div id="box">H2H</div>
+            <p>咱的好伴手</p>
+            </div><!-- /logo -->
+    </div><!-- /subContainer -->
+</div><!-- header -->
+    
+<div id="navigation">   
+	<ul id="sddm">
+        <li><a href="index.php">首頁</a></li>
+        <li><a href="product.php">商品資料</a></li>
+        <li><a href="cart.php">購物車</a></li>
+        <li><a href="checkOut.php">結帳</a></li>     
+        <li><a href="guestBook.php">留言板</a></li>        
+        <li><a href="titleView.php">討論區</a></li>
+	    <li><a href="#" 
+        onmouseover="mopen('m1')" 
+        onmouseout="mclosetime()">會員中心</a>
+        <div id="m1" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="reconfirm.php">重發確認信</a>
+        <a href="modifydata.php">修改基本資料</a>
+        <a href="forgetpassword.php">忘記密碼</a>
+        </div>
+    	</li>
+	    <li><a href="#" 
+        onmouseover="mopen('m2')" 
+        onmouseout="mclosetime()">公佈欄</a>
+        <div id="m2" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="publishBrowser.php">瀏覽公佈欄</a>
+        </div>
+    	</li>
+		</ul> 
+<div style="clear:both"></div>    
+</div><!-- /navigation -->
+<object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="780" height="120">
+    <param name="movie" value="../Scripts/pic.swf" />
+    <param name="quality" value="high" />
+    <param name="wmode" value="opaque" />
+    <param name="swfversion" value="8.0.35.0" />
+
+    <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+    <!--[if !IE]>-->
+    <object type="application/x-shockwave-flash" data="../Scripts/pic.swf" width="780" height="120">
+      <!--<![endif]-->
+      <param name="quality" value="high" />
+      <param name="wmode" value="opaque" />
+      <param name="swfversion" value="8.0.35.0" />
+      <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+      <div>
+        <h4>這個頁面上的內容需要較新版本的 Adobe Flash Player。</h4>
+        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="取得 Adobe Flash Player" width="112" height="33" /></a></p>
+      </div>
+      <!--[if !IE]>-->
+    </object>
+    <!--<![endif]-->
+  </object>
+<div id="container"><!-- InstanceBeginEditable name="EditRegion1" -->
+  <div id="primaryContent">
+    <h2>討論區</h2>
+    <h3>您可以在此瀏覽您有興趣的主題並參與討論！</h3>
+    
+    <p>&nbsp;</p>
+    <a href="createTitle.php">我有討論</a>
+    <p>&nbsp;</p>
+    <table border="1" width="100%">
+    <tr>
+    <td>主題名稱</td>
+    <td>作者姓名</td>
+    <td>回應數量</td>
+    <td>建立日期/時間</td>
+    </tr>
+    <?php do { ?>
+  <tr>
+    <td><h3><?php echo $row_Recordset1['subject']; ?></h3></td>
+    <td><h4><?php echo $row_Recordset1['name']; ?></h4></td>
+    <td><h5><?php echo $row_Recordset1['count']; ?></h5></td>
+    <td><h6><?php echo $row_Recordset1['createDate']; ?></h6></td>
+  </tr>
+  <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
+    </table>
+    <p>&nbsp;</p>
+    <hr>
+    <p>&nbsp;</p>
+        [<a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, 0, $queryString_Recordset1); ?>">第一頁</a>]
+        [<a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, max(0, $pageNum_Recordset1 - 1), $queryString_Recordset1); ?>">上一頁</a>]
+        [<a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, min($totalPages_Recordset1, $pageNum_Recordset1 + 1), $queryString_Recordset1); ?>">下一頁</a>]
+        [<a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, $totalPages_Recordset1, $queryString_Recordset1); ?>">最後一頁</a>]
+        
+        <p>第 <?php echo ($startRow_Recordset1 + 1) ?> 筆至第 <?php echo min($startRow_Recordset1 + $maxRows_Recordset1, $totalRows_Recordset1) ?> 筆/共 <?php echo $totalRows_Recordset1 ?>  筆</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <!-- /comments -->
+  </div>
+<!-- InstanceEndEditable -->
+  <!-- /primaryContent -->
+  <div id="secondaryContent">
+    <p>&nbsp;</p>
+    <h3>最新商品</h3>
+    <h3>相關連結</h3>
+    <!-- 
+            This part was designed to handle images
+            -->
+    <ul class="links">
+      <li><a href="http://www.pcschool.com.tw/">巨匠電腦</a></li>
+      <li><a href="http://kid.pcschool.com.tw/">兒童電腦</a></li>      
+      <li><a href="http://www.soeasyedu.com.tw/">數位學習</a></li>
+      <li><a href="http://www.soeasyedu.com.tw/">巨匠美語</a></li>  
+      <li><a href="http://www.off.com.tw/weiblog">給我建議</a>
+    </ul>
+    <h3>登入</h3>
+    <form action="" method="post">
+      <p>
+        <input value="user" name="user" />
+      </p>
+      <p>
+        <input name="password" type="password" value="password" />
+      </p>
+        <button type="submit">送出</button><button>註冊會員</button>        
+    </form>    
+  </div>
+  <!-- /secondaryContent -->
+        <br class="clear" />    
+</div><!-- container -->
+    
+    <div id="footer">
+        <ul>
+            <li id="copyright">&copy; 2099 Free for Gjun student member</li>
+            <li id="links">
+                <ul>
+                    <li><a href="#">XHTML</a> |</li>
+                    <li><a href="#">CSS</a> |</li>
+                    <li><a href="http://gjun.tw">ALAN CHEN</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div><!-- /footer -->
+</body>
+<!-- InstanceEnd --></html>
+<?php
+mysql_free_result($Recordset1);
+?>
